49003ab4d1cc16ff2be9332fb443c615b91f0bf8
zdy <boxuerixin@gmail.com>
2017-12-04 14:31:45 +0800

    七牛云上传图片

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'ACCESS_KEY': 'KeaTSIB5rQLWgdjGgf0eVPVr-sb-fAJRz7bxYjqh', // https://portal.qiniu.com/user/key
+  'SECRET_KEY': 'XkxMvEXSGfkjZWYm9PlgyMol-39ChHB7Wti-1_jh',
+  'Bucket_Name': 'zhangdanyang',
+  'Port': 1225,
+  'Uptoken_Url': 'uptoken',
+  'Domain': 'http://orvm1p4c8.bkt.clouddn.com/'// bucket domain eg:http://qiniu-plupload.qiniudn.com/
+};
