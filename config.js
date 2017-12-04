5e5679a635b68404f9b3e5a2d050c49afb7a8916
zdy <boxuerixin@gmail.com>
2017-12-04 15:21:23 +0800

    update

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   'ACCESS_KEY': 'KeaTSIB5rQLWgdjGgf0eVPVr-sb-fAJRz7bxYjqh', // https://portal.qiniu.com/user/key
   'SECRET_KEY': 'XkxMvEXSGfkjZWYm9PlgyMol-39ChHB7Wti-1_jh',
   'Bucket_Name': 'zhangdanyang',
-  'Port': 1225,
+  'Port': 8888,
   'Uptoken_Url': 'uptoken',
   'Domain': 'http://orvm1p4c8.bkt.clouddn.com/'// bucket domain eg:http://qiniu-plupload.qiniudn.com/
 };
